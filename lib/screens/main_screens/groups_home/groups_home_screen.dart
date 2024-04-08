9a4a37ee9b2751a5541e7640285b709a7850bbe2
Mohammed Tharwat <117047676+MoAce8@users.noreply.github.com>
2024-04-08 16:28:33 +0200

    Add files via upload

---
+++ lib/screens/main_screens/groups_home/groups_home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:ace_chat_app/screens/main_screens/chat_home/widgets/chat_card.dart';
+import 'package:flutter/material.dart';
+
+class GroupsHomeScreen extends StatelessWidget {
+  const GroupsHomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Groups'),
+        surfaceTintColor: Colors.transparent,
+      ),
+      body: ListView.builder(
+        itemCount: 8,
+        itemBuilder: (context, index) => const ChatCard(),
+      ),
+    );
+  }
+}
