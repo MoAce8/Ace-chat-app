3c72a84da897c5c81eb0e8c3f5f43e6203ef2ccf
MoAce8 <mohammedtharwat21@gmail.com>
2024-05-21 12:24:35 +0300

    go to chat screen from contacts

--- lib/screens/main_screens/contacts_home/widgets/contact_card.dart
+++ lib/screens/main_screens/contacts_home/widgets/contact_card.dart
@@ -1,4 +1,6 @@
+import 'package:ace_chat_app/firebase/fire_database.dart';
 import 'package:ace_chat_app/models/user_model.dart';
+import 'package:ace_chat_app/screens/chat/chat_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -12,11 +14,27 @@ class ContactCard extends StatelessWidget {
     return Card(
       child: InkWell(
         borderRadius: BorderRadius.circular(12),
-        onTap: () {},
+        onTap: () {
+          List<String> members = [
+            user.id,
+            FirebaseAuth.instance.currentUser!.uid
+          ]..sort(
+              (a, b) => a.compareTo(b),
+            );
+          FireData()
+              .createRoom(context, email: user.email)
+              .then((value) => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        ChatScreen(roomId: members.toString(), user: user),
+                  )));
+        },
         child: ListTile(
           title: Text(
-            user.id!=FirebaseAuth.instance.currentUser!.uid?
-            user.name:'${user.name} (You)',
+            user.id != FirebaseAuth.instance.currentUser!.uid
+                ? user.name
+                : '${user.name} (You)',
           ),
           trailing: const Icon(FontAwesomeIcons.commentDots),
         ),
